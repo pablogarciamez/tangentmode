cefcd0ef9c2e3a9d84787fa5098a002f72a17a5a
pablogarciamez <pablogarciamez@gmail.com>
2026-09-15 16:11:11 +0200

    Implementado suma, multiplicación, resta, división, igualdad...

--- src/tangentmode/dual.py
+++ src/tangentmode/dual.py
@@ -1,8 +1,52 @@
+
+
 class Dual:
+    @staticmethod
+    def _to_dual(value):
+        if isinstance(value, Dual):
+            return value
+        if isinstance(value, (int, float)):
+            return Dual(value, 0)
+        return NotImplemented
+
     def __init__(self, real, dual):
         self.real = real
         self.dual = dual
+
+    def __neg__(self):
+        return Dual(-self.real, -self.dual)
+
+    def __add__(self, other):
+        other = Dual._to_dual(other)
+        return Dual(self.real + other.real, self.dual + other.dual)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        other = Dual._to_dual(other)
+        return Dual(self.real - other.real, self.dual - other.dual)
+
+    def __rsub__(self, other):
+        return -(self - other)
+
     def __mul__(self, other):
-        return Dual(self.real * other.real, self.real * other. dual + self.dual * other.real)
+        other = Dual._to_dual(other)
+        return Dual(self.real * other.real, self.real * other.dual + self.dual * other.real)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        other = Dual._to_dual(other)
+        return Dual(self.real / other.real, (self.dual * other.real - self.real * other.dual) / (other.real ** 2))
+
+    def __rtruediv__(self, other):
+        return Dual(other, 0) / self
+
+    def __eq__(self, other):
+        other = Dual._to_dual(other)
+        return self.real == other.real and self.dual == other.dual
+
     def __repr__(self):
         return f"Dual({self.real}, {self.dual})"
--- tests/test_duals.py
+++ tests/test_duals.py
@@ -11,4 +11,25 @@ def test_dual_squared():
     def f(x): return x*x
     x = Dual(4,3)
     y = f(x)
-    assert y != Dual(16, 24)
+    assert y == Dual(16, 24)
+
+def test_polinomial_function():
+    def f(x): return 3*x*x + 2*x + 5
+    x = Dual(1, 1)
+    y = f(x)
+    assert y.dual == 8
+
+def test_commutative_property():
+    x = Dual(2, 3)
+    assert 2 * x == x * 2
+
+def test_division():
+    x = Dual(1, 2)
+    y = Dual(2, 3)
+    assert x / y == Dual(1 / 2, 1 / 4)
+
+def test_rsub():
+    assert 3 - Dual(2, 1) == Dual(1, -1)
+
+def test_rdiv():
+    assert 3 / Dual(2, 1) == Dual(3 / 2, -3 / 4)
